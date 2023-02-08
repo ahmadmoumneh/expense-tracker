f0c49e954012580033b88e31762b1de777a77efd
Ahmad Moumneh <ahmad.moumneh@hotmail.com>
2023-02-07 21:23:39 -0500

    initial commit

---
+++ src/main/java/am/software/expensetracker/ExpenseTracker.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package am.software.expensetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author ahmad
+ */
+@SpringBootApplication
+public class ExpenseTracker {
+    public static void main(String[] args) {
+        SpringApplication.run(ExpenseTracker.class, args);  
+    }
+}
